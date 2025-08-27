9c1a55839bfd6d51d68f5f2a8ad9b7976b1ed2f3
Vipin Singh <vipin.singh632@gmail.com>
2025-08-27 16:20:10 +0530

    teck stack updates

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
+// import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
   DiGit,
- 
+  DiPython,
+  // DiMysql,
 } from "react-icons/di";
 import {
- 
-  SiFirebase,
+  // SiFirebase,
   SiNextdotjs,
- 
+  SiExpress,
+  SiTypescript,
 } from "react-icons/si"; 
 import { IoLogoFirebase } from "react-icons/io5";
+import { AiOutlineConsoleSql } from "react-icons/ai";
 // import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
@@ -41,6 +43,18 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
       <IoLogoFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiExpress />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTypescript />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <AiOutlineConsoleSql />
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { DiVisualstudio } from "react-icons/di";
 import { SiPostman } from "react-icons/si";
+import { SiAmazonwebservices } from "react-icons/si";
 import { FaSlack } from "react-icons/fa";
 import { RiVercelLine } from "react-icons/ri";
 import { SiMacos } from "react-icons/si";
+import { SiRender } from "react-icons/si";
 
 function Toolstack() {
   return (
@@ -23,6 +25,12 @@ function Toolstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <RiVercelLine />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+       <SiAmazonwebservices />
+     </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRender />
       </Col>
     </Row>
   );
